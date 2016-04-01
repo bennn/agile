70401b18ceabcf537d4fb645d0818a238c8533c0
Alex Knauth <alexander@knauth.org>
2016-04-01 13:42:53 -0400

    Add support for defining simple macros

--- main.rkt
+++ main.rkt
@@ -1,4 +1,5 @@
 #lang racket/base
-(require (for-syntax racket/base syntax/parse))
-(provide (all-from-out racket/base)
-         (for-syntax (all-from-out racket/base) (all-from-out syntax/parse)))
+(require syntax/parse/define
+         (for-syntax racket/base syntax/parse))
+(provide (all-from-out racket/base syntax/parse/define)
+         (for-syntax (all-from-out racket/base syntax/parse)))
